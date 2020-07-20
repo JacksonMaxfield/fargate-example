d54ba7966833086aa90f2951b25de4106792f97c
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-07-20 16:12:10 -0700

    Include prefect example

--- prefect_example.py
+++ prefect_example.py
@@ -3,9 +3,12 @@
 
 import logging
 import time
+from typing import List
 
 from dask_cloudprovider import FargateCluster
 from distributed import Client
+from prefect import Flow, task
+from prefect.engine.executors import DaskExecutor
 
 ###############################################################################
 
@@ -17,13 +20,19 @@ log = logging.getLogger(__name__)
 
 ###############################################################################
 
+@task
 def add_one(val: int) -> int:
     time.sleep(1)
     return val + 1
 
+@task
 def multiply_by_ten(val: int) -> int:
     time.sleep(1)
     return val * 10
+
+@task
+def sort_values(values: List[int]) -> List[int]:
+    return sorted(values)
 
 
 if __name__ == "__main__":
@@ -36,24 +45,26 @@ if __name__ == "__main__":
 
     log.info(f"Dashboard available at: {cluster.dashboard_link}")
     cluster.adapt(minimum=10, maximum=30)
-    client = Client(cluster.scheduler_address)
 
-    # Actual work
     initial_values = list(range(1000))
     log.info(f"Values range: [{initial_values[0]}-{initial_values[-1]}]")
 
-    # Add
-    addition_futures = client.map(add_one, initial_values)
-    values = client.gather(addition_futures)
+    # Generate the DAG
+    with Flow("My Workflow") as flow:
+        values = add_one.map(initial_values)
+        values = multiply_by_ten.map(values)
+        sort_values(values)
 
-    # Multiply
-    multiplication_futures = client.map(multiply_by_ten, values)
-    values = client.gather(multiplication_futures)
-
-    # Sort
-    values = sorted(values)
+    # Run the Workflow / DAG
+    state = flow.run(executor=DaskExecutor(address=cluster.scheduler_address))
 
     # Results
-    log.info(f"Values range: [{values[0]}-{values[-1]}]")
+    # This returns all values that match the task name "sort_values"
+    result_values = [
+        state.result[flow_task].result for
+        flow_task in flow.get_tasks(name="sort_values")
+    ][0]  # since there is only one task with that name, return the first one
+
+    log.info(f"Values range: [{result_values[0]}-{result_values[-1]}]")
 
     log.info("All done!")
